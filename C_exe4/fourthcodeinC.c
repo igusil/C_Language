999b90118fb81570fa68d9f88d8f5df8fc59b6df
ig0r <igusil@outlook.com>
2023-07-11 22:16:16 -0300

    goto JMP

---
+++ C_exe4/fourthcodeinC.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main(void)
+{
+  char name[50];
+  int x;
+
+  // 49chars
+  scanf("%49s", name);
+
+  for (x = 0; x < 50; x++)
+  {
+    if (name[x] == 0)
+      break;
+    if (name[x] == 'a')
+      continue;
+    printf("%c", name[x]);
+  }
+  printf("\n");
+  return 0;
+}
+
+/*
+#include <stdio.h>
+int main (void) {,
+    int x=0;
+
+    Aqui:
+        printf("%X",x);
+        x++;
+        if (x<15)
+            goto Aqui;
+        printf("end");
+
+        return 0;
+}
+
+//goto debuging with IDA, example goto, JMP assembly
+*/
