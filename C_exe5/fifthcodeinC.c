c9af8613a6f3519838a9f748f82d13074a0a74b9
ig0r <igusil@outlook.com>
2023-07-11 22:59:48 -0300

    encrypt and decrypt c

---
+++ C_exe5/fifthcodeinC.c
@@ -0,0 +1,29 @@
+// password encrypt and decrypt
+
+#include <stdio.h>
+
+int main(void)
+{
+  char name[10];
+  int pass;
+
+  printf("Enter your name: ");
+  scanf("%8s", &name);
+
+  printf("%8s\n", name);
+
+  printf("Enter your pass: ");
+  scanf("%d", &pass);
+
+  printf("Default pass: %d", pass);
+
+  int new = pass += 12;
+
+  printf("\nEncrypted pass: %d", new);
+
+  int decrypt = pass -= 12;
+
+  printf("\nDecrypted pass: %d", decrypt);
+
+  return 0;
+}
