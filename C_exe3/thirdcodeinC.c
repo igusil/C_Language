157bc311cd266f3c4d75c7772bcb222b2e0945f3
igusil <igusil@outlook.com>
2023-07-24 22:47:35 -0300

    calc with vector

--- C_exe3/thirdcodeinC.c
+++ C_exe3/thirdcodeinC.c
@@ -41,3 +41,48 @@ int main(void)
 
   return 0;
 }
+
+/*  #include <stdio.h>
+    #include <limits.h>
+  3 #include <stdint.h>
+  4 #include <stdlib.h>
+  5
+  6 int main (void) {
+  7   int n[2];
+  8   int sum,sub,multi,div,res;
+  9   char operator[5];
+ 10   char equation;
+ 11
+ 12   printf("Enter your option(+ , - , * , / ): ");
+ 13   scanf("%c", &equation);
+ 14
+ 15   printf("Enter your first number: ");
+ 16   scanf("%d", &n[0]);
+ 17   printf("Enter your second number: ");
+ 18   scanf("%d", &n[1]);
+ 19
+ 20   if(equation == '+') {
+ 21     scanf("%c", &operator[0]);
+ 22     sum = n[0] + n[1];
+ 23     printf("The result of %d and %d is: %d\n",n[0],n[1],sum);
+ 24   }
+ 25   else if (equation == '-') {
+ 26     scanf("%c", &operator[1]);
+ 27     sub = n[0] - n[1];
+ 28     printf("The result of %d and %d is: %d\n", n[0],n[1],sub);
+ 29   }
+ 30   else if (equation == '*') {
+ 31     scanf("%c", &operator[2]);
+ 32     multi = n[0] * n[1];
+ 33     printf("The result of %d and %d is %d", n[0],n[1],multi);
+ 34   }
+ 35   else if (equation == '/') {
+ 36     scanf("%c", &operator[3]);
+ 37     div = n[0] / n[1];
+ 38     printf("The result of %d and %d is %d", n[0],n[1],div);
+ 39   }
+ 40
+ 41   return 0;
+ 42 }
+
+*/
