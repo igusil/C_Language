045c7fa451d216c56100dc04ccc00c172aecce5c
igusil <igusil@outlook.com>
2023-08-03 21:56:47 -0300

    loop

--- C_exe17/seventhcodeinC.c
+++ C_exe17/seventhcodeinC.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <stdint.h>
- 
-int main (void) {
-  //unsigned int i; //inteiro positivo, sem sinal
+
+int main(void)
+{
+  // unsigned int i; //inteiro positivo, sem sinal
   /*
   print:
   printf("O valor de i eh: %u\n", i);
@@ -12,9 +13,35 @@ int main (void) {
     if (i < 3)
      goto print;
   */
-  for (unsigned int i = 0 ; i < 7 ; i++) {
+  for (unsigned int i = 0; i < 7; i++)
+  {
     printf("O valor de i eh: %u\n", i);
   }
 
   return 0;
- }
+}
+
+/*
+1 #include <stdio.h>
+ 2 #include <stdlib.h>
+ 3 #include <stdint.h>
+ 4 #include <limits.h>
+ 5
+ 6 int main (void) {
+ 7   unsigned int i = 0;
+ 8
+ 9   for ( ; i < 3 ; i++) {
+10     printf("O valor de i eh: %u\n", i);
+11
+12   }
+13   while (i < 2) {
+14     printf("O valor de i eh: %u\n", i++);
+15   }
+16
+17   return 0;
+18 }
+
+
+
+
+*/
