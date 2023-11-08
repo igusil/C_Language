f4fb088cbf4b6bf621d2c79d0ff086183845b96e
igusil <igusil@outlook.com>
2023-11-07 23:25:20 -0300

    newf

--- C_exe22/if.c
+++ C_exe22/if.c
@@ -2,13 +2,14 @@
 
 int main(void) {
   int a = 359;
-
+  
+  if (-1 == a)
   {
     int chave = 3;
     printf("chave: %d\n", chave);
   }
 
-  printf("O resultado eh %d\n", a ^ chave);
+  printf("a: %d\n", a);
 
   return(0);
 }
