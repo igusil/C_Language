57d9b2d264dc75a88aa9898d5877a54e3411e896
igusil <igusil@outlook.com>
2023-11-09 22:57:33 -0300

    newf

--- C_exe22/if.c
+++ C_exe22/if.c
@@ -1,12 +1,16 @@
-i#include <stdio.h>
+ #include <stdio.h>
 
 int main(void) {
   int a = 359;
-  
-  if (-1 == a || a < 400) {
-    int chave = 3;
+  int chave = 3;
+
+  if (-1 == a && a < 400) {
+    
     printf("chave: %d\n", chave);
+    printf("oi\n");
   }
+  else  
+    printf("avaliacao do if deu FALSE\n");
 
   printf("a: %d\n", a);
 
