bf23a84fed53e09e8abef5202caa2f88e99243f9
igusil <igusil@outlook.com>
2023-11-30 21:18:21 -0300

    func1

---
+++ C_exe23/func1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int num1, num2, num3;
+
+  printf("Enter your num1: ");
+  scanf("%d", &num1);
+  printf("Enter your num2: ");
+  scanf("%d", &num2);
+  printf("Enter your num3: ");
+  scanf("%d", &num3);
+
+  printf("num1: %d - num2: %d\n", num1, num2, num3);
+
+  return(0);
+}
