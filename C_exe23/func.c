9564586be2af844815d150ae375eab7abd5b970e
igusil <igusil@outlook.com>
2023-11-30 21:06:22 -0300

    func

--- C_exe23/func.c
+++ C_exe23/func.c
@@ -2,7 +2,21 @@
 
 int main(void) {
 
-  printf("soma eh %d\n", 3 + 5 + 5);
+  int num1, num2, num3;
+  printf("Digiteo num 1: ");
+  scanf("%d", &num1);
+  printf("%d\n",num1);
 
+  printf("Digite o num 2: ");
+  scanf("%d", &num2);
+  printf("%d\n", num2);
+
+  printf("Digite o num 3: ");
+  scanf("%d\n", &num3);
+  printf("%d\n", num3);
+
+   //printf("soma eh %d\n", 3 + 5 + 5);
+
+   //printf("num1: %d\n", num1);
   return(0);
 }
