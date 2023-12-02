9506c8ddd672c44e6487d3fd01b57691db0e954b
igusil <igusil@outlook.com>
2023-12-01 23:52:45 -0300

    func1

--- C_exe23/func1.c
+++ C_exe23/func1.c
@@ -11,7 +11,7 @@ int main(void) {
   printf("Enter your num3: ");
   scanf("%d", &num3);
 
-  printf("num1: %d - num2: %d\n", num1, num2, num3);
+  printf("soma: %d\n", num1 + num2 + num3);
 
   return(0);
 }
