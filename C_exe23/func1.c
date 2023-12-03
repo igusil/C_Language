2b93a22d54dc2fd5e34b760222bcc91240d27747
igusil <igusil@outlook.com>
2023-12-03 00:34:40 -0300

    func1

--- C_exe23/func1.c
+++ C_exe23/func1.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+//tipo_de_retorno nome(tipo_arg nome_arg, tipo_arg1, tipo_arg2, nome_arg2)
+
+int soma(int n1, int n2, int n3) {
+  int res;
+  res n1 + n2 + n3;
+  return res;
+}
+
+
 int main(void) {
 
   int num1, num2, num3;
@@ -10,6 +19,8 @@ int main(void) {
   scanf("%d", &num2);
   printf("Enter your num3: ");
   scanf("%d", &num3);
+  
+  printf("soma: %d\n", soma(num1, num2, num3);
 
   printf("soma: %d\n", num1 + num2 + num3);
 
