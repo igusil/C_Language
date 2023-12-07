f987e7019fb26fa4bff57b345d5e03037f670950
igusil <igusil@outlook.com>
2023-12-06 22:43:22 -0300

    func2

--- C_exe23/func2.c
+++ C_exe23/func2.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 void tchau(char msg[]){
-  printf(msg);
+  fprintf(stderr, "ERRO: %s\n", msg);
   exit(1);
 }
 
@@ -13,9 +13,9 @@ int main (void){
   scanf("%d", &num1);
 
   if (num1 < 1)
-    tchau("Erro: o num1 eh menor que 1\n");
+    tchau("o num1 eh menor que 1\n");
   else if (num1 < 1900)
-    tchau("vc eh mto velho\n");
+//    tchau("vc eh mto velho\n");
 
   printf("Voce nasceu em %d\n", num1);
 
